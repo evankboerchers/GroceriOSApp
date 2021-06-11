74dda90eb07cb00f5c384979653bffc158e6414c
Evan Boerchers <eboer@Evans-iMac.local>
2021-06-10 17:21:07 -0700

    initial commit

---
+++ Grocer/Grocer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Grocer
+//
+//  Created by Evan Boerchers on 2021-06-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Grocer/Grocer/GrocerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrocerApp.swift
+//  Grocer
+//
+//  Created by Evan Boerchers on 2021-06-10.
+//
+
+import SwiftUI
+
+@main
+struct GrocerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
