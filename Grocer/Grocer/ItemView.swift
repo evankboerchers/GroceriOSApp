905d327146e3c9b327bbb067f15154b656f46a66
Evan Boerchers <evanboerchers@Evans-iMac.local>
2021-06-14 18:07:00 -0700

    completed list editing and added views for grocery list and grocery item

---
+++ Grocer/Grocer/ItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemView.swift
+//  Grocer
+//
+//  Created by Evan Boerchers on 2021-06-14.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemView()
+    }
+}
---
+++ Grocer/Grocer/ListView.swift
@@ -0,0 +1,20 @@
+//
+//  ListView.swift
+//  Grocer
+//
+//  Created by Evan Boerchers on 2021-06-14.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
